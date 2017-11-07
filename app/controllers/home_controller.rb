6dcd786fd8345ec8937863927546d24568ba309e
nine-god <191176233@qq.com>
2017-11-07 19:59:24 +0800

    完成初稿

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def show
   end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :news
+  resources :products
   get 'home/show'
-  
+  get 'home/about' 
   root "home#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
