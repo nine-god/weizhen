e11db1a9a52646afbfcdc70bfd2eab6680d4d2c4
nine-god <191176233@qq.com>
2017-11-26 14:14:07 +0800

    移除调试

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,17 +23,9 @@ function init_ueditor(){
 catch(err)
   {
   	// var ue = UE.getEditor('ueditor_container')
-  	console.log(err)
+  	// console.log(err)
   	// console.log(111)
   //在这里处理错误
   }
 
-  console.log(333)
-    // ue.render('ueditor_container');
-  // ue = UE.getEditor('ueditor_container')
-  // console.log(ue)
-  // if(ue){
-    // var ue = UE.getEditor('ueditor_container').render('ueditor_container');
-  // }
-  // console.log(222)
 };
