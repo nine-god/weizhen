5c6cf6c6b0659dcf7a8a19f27884af56c88710df
nine-god <191176233@qq.com>
2017-11-29 19:53:13 +0800

    更新预编译文件

--- public/assets/app-4e6666abae5e976b3bdc0bb72faf36c18d588b48557045db2d027bf1b569c48f.js
+++ public/assets/app-4e6666abae5e976b3bdc0bb72faf36c18d588b48557045db2d027bf1b569c48f.js
@@ -722,6 +722,10 @@ catch(err)
   }
   // console.log(UE.getEditor('ueditor_container').getOpt('serverUrl'))
 };
+function sync_home_profile(){
+	var ue = UE.getEditor('ueditor_container');
+	$('textarea[name="home[profile]"]').html(ue.getContent());
+};
 function sync_product_profile(){
 	var ue = UE.getEditor('ueditor_container');
 	$('textarea[name="product[profile]"]').html(ue.getContent());
